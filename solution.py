3eacd8f478d486836d3eea47fe85893960ff1212
Ress <mail@ress.ws>
2022-09-30 17:07:45 +0200

    basic stepover flights search

--- solution.py
+++ solution.py
@@ -34,23 +34,75 @@ def read_csv(file) -> List[Dict]:
     return list
 
 def filter_flights(data, origin, destination, bags) -> List[Dict]:
-    filtered = filter(lambda item: item["origin"] == origin or item["destination"] == destination, data)
-    filtered = filter(lambda item: int(item["bags_allowed"]) >= bags, filtered)
-    return list(filtered)
+    # filtered = filter(lambda item: item["origin"] == origin or item["destination"] == destination, data)
+    filtered = filter(lambda item: int(item["bags_allowed"]) >= bags, data)
+    ls = list(filtered)
+    print(f"filter_flights: { len(ls)}")
+    return ls
 
 def search_combinations(flights, origin, destination):
     bin = []
 
-    for flight in flights:
-        print(flight)
+    def compare_origin_destination(flights, origin, destination) -> Optional[Dict]:
+        for flight in flights:
+            # print(flight)
+            if flight["origin"] == origin and flight["destination"] == destination:
+                # bin.append(f"{flight['origin']} > {flight['destination']}")
+                return flight
+        return None
+
+    # search direct flights:
+    for index, flight in enumerate(flights):
+        chain = []
+        # print(index)
         if flight["origin"] == origin and flight["destination"] == destination:
-            bin.append(f"{origin} > {destination}")
-        else:
-            if flight["origin"] == origin:
-                bin.append(f"{origin} > {flight['destination']}")
+            verbal = f"Direct: {origin} > {destination}"
+            chain = [verbal, flight]
+            bin.append(chain)
+            flights.pop(index)
+
+    # search stepover flights:
+    for index, flight in enumerate(flights):
+        chain = []
+        # print(index)
+        # if flight["origin"] == origin and flight["destination"] == destination:
+        #     verbal = f"Direct: {origin} > {destination}"
+        #     chain = [verbal, flight]
+        #     bin.append(chain)
+        #     flights.pop(index)
+        #     continue
+        if flight["origin"] == origin:
+                # print("AAA")
+                pair = compare_origin_destination(flights, flight["destination"], destination)
+                if pair:
+                    verbal = f"Stepover: {flight['origin']} > {pair['origin']} > {pair['destination']}"
+                    chain = [verbal, flight, pair]
+                    bin.append(chain)
+                    flights.pop(index)
+                    continue
+                    # flights.pop(index)
+        elif flight["destination"] == destination:
+                # print("BBB")
+                pair = compare_origin_destination(flights, origin, flight["destination"])
+                if pair:
+                    # chain += f"Destination: {pair['origin']} > {pair['destination']}"
+                    verbal = f"Stepover: {pair['origin']} > {flight['origin']} > {flight['destination']}"
+                    chain = [verbal, pair, flight]
+                    bin.append(chain)
+                    flights.pop(index)
+                    continue
+                    # flights.pop(index)
+
+            # print(f"- miss {flight['origin']} > {flight['destination']}")
+            
+        if len(chain) > 0:
+            bin.append(chain)
 
     for item in bin:
+        print("---------")
         print(item)
+
+    print(f"search_combinations: { len(bin)}")
 
     return bin
 
@@ -60,8 +112,7 @@ if __name__ == '__main__':
     data = read_csv(datafile)
 
     filtered = filter_flights(data, origin, destination, bags)
-    # print(filtered)
-    print(len(filtered))
+    # print(len(filtered))
     zzz = search_combinations(filtered, origin, destination)
 
 # print(args.datafile)
